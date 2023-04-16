e74108f1d52c014de9ad8ed6953e40f282a60284
luboszavarsky <zavarsky.lubos@gmail.com>
2023-04-16 20:52:30 +0200

    get route tst

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("test");
+import express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/api/songs", (req, res) => {
+  res.status(200).json({ message: "Song sent" });
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}!`));
